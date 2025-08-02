bbb1dee7c80cc5aab7710c12f6493714df3ce2b9
jinhai-cloud <jinhai.cloud@gmail.com>
2025-08-02 15:29:44 +0800

    add human in the loop

--- src/agents/react.py
+++ src/agents/react.py
@@ -2,9 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_tavily import TavilySearch
 from langgraph.prebuilt import create_react_agent
+from langgraph.types import interrupt
 
 load_dotenv()
 
@@ -14,6 +16,12 @@ llm = init_chat_model(
     api_key=os.getenv("DASHSCOPE_API_KEY"),
     base_url=os.getenv("DASHSCOPE_BASE_URL")
 )
+
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
 
 os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
 tavily_tool = TavilySearch(max_results=5)
@@ -27,6 +35,5 @@ async def make_graph():
             }
         }
     )
-    tools = await client.get_tools()
-    tools.append(tavily_tool)
+    tools = [tavily_tool, human_assistance] + await client.get_tools()
     return create_react_agent(llm, tools)
